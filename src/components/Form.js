5a18de70a539b8d9b3474cb6a73a3330a641d8c8
Santiago_Albisser <salbisserc@eafit.edu.co>
2022-06-18 12:15:50 -0500

    responsive
    Signed-off-by: Santiago_Albisser <salbisserc@eafit.edu.co>

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,107 +1,114 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components';
+import { useState } from 'react'
+import styled from 'styled-components'
 
+import { addDoc, collection } from 'firebase/firestore'
 import { db } from '../database/Firebase'
-import {
-    collection,
-    addDoc
-} from 'firebase/firestore'
-import AnimatedButton from './AnimatedButton';
-
+import AnimatedButton from './AnimatedButton'
 
 const Form = () => {
-    const valorInicial = {
-        nombre: '',
-        email: '',
-        numero: ''
-    }
-    const [user, setUser] = useState(valorInicial)
+  const valorInicial = {
+    nombre: '',
+    email: '',
+    numero: ''
+  }
+  const [user, setUser] = useState(valorInicial)
 
-    const capturarInputs = e => {
-        const { name, value } = e.target
-        setUser({ ...user, [name]: value })
-    }
-    const guardarDatos = async e => {
-        e.preventDefault()
-        console.log(user)
-        setLoader(true);
+  const capturarInputs = e => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+  }
+  const guardarDatos = async e => {
+    e.preventDefault()
+    console.log(user)
+    setLoader(true)
 
-        addDoc(collection(db, 'registro'), {
-            ...user
-        })
-            .then(() => {
-                setLoader(false)
-                alert('Estas inscrito a la lista de espera.👍')
-            })
-            .catch(error => {
-                alert(error.message)
-                setLoader(false)
-            })
+    addDoc(collection(db, 'registro'), {
+      ...user
+    })
+      .then(() => {
+        setLoader(false)
+        alert('Estas inscrito a la lista de espera.👍')
+      })
+      .catch(error => {
+        alert(error.message)
+        setLoader(false)
+      })
 
-        setUser({ ...valorInicial })
-    };
+    setUser({ ...valorInicial })
+  }
 
-    const [loader, setLoader] = useState(false)
+  const [loader, setLoader] = useState(false)
 
-    return (
-        <FormStyles>
-        <form className='form' onSubmit={guardarDatos}>
-        <h4 className="secondary-heading">
-            Registrate en la lista de espera.</h4>
-            <div className='form-register'>
-            <input
-                type='text'
-                name='nombre'
-                placeholder='Ingresa tu nombre'
-                onChange={capturarInputs}
-                value={user.nombre}
-            />
-            <input
-                type='text'
-                name='email'
-                placeholder='Ingresa tu email'
-                onChange={capturarInputs}
-                value={user.email}
-            />
-            <input
-                type='text'
-                name='numero'
-                placeholder='Ingresa tu numero'
-                onChange={capturarInputs}
-                value={user.numero}
-            />
-            <AnimatedButton className='btn' name={'Enviar'} />
-            </div>
-        </form>
-        </FormStyles>
-    )
+  return (
+    <FormStyles>
+      <form className='form' onSubmit={guardarDatos}>
+        <h4 className='secondary-heading'>Registrate en la lista de espera.</h4>
+        <div className='form-register'>
+          <input
+            type='text'
+            name='nombre'
+            placeholder='Ingresa tu nombre'
+            onChange={capturarInputs}
+            value={user.nombre}
+          />
+          <input
+            type='text'
+            name='email'
+            placeholder='Ingresa tu email'
+            onChange={capturarInputs}
+            value={user.email}
+          />
+          <input
+            type='text'
+            name='numero'
+            placeholder='Ingresa tu numero'
+            onChange={capturarInputs}
+            value={user.numero}
+          />
+          <AnimatedButton className='btn' name={'Enviar'} />
+        </div>
+      </form>
+    </FormStyles>
+  )
 }
 
 const FormStyles = styled.div`
-    .form-register{
-        display: grid;
-        grid-template-columns: repeat(1, 1fr);
-        padding: 2rem;
-    }
-    input{
-        border-radius: 20px;
-        border: 1px solid var(--border-colour); 
-        padding: 0.5rem;
-        margin: 0.5rem;
-    }
-    background-color: white;
-    border-radius: 50px;
-    border: 1px solid var(--border-colour);
-    height: 25rem;
+  @media screen and (max-width: 1347px) {
+    padding: 5rem 14rem;
+  }
+  @media screen and (max-width: 1186px) {
+    padding: 5rem 8rem;
+  }
+  @media screen and (max-width: 990px) {
+    padding: 5rem 4rem;
+  }
+  @media screen and (max-width: 760px) {
+    padding: 5rem 4rem;
+  }
+  .form-register {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
     padding: 2rem;
-    box-shadow: 0px 25px 50px rgba(22, 25, 79, 0.05);
-    h4{
-        font-size: 3rem;
-        color: var(--purple-primary);
-    }
-    p{
-        color: black;
-    }
-`;
+  }
+  input {
+    border-radius: 20px;
+    border: 1px solid var(--border-colour);
+    padding: 0.5rem;
+    margin: 0.5rem;
+  }
+  background-color: white;
+  border-radius: 50px;
+  border: 1px solid var(--border-colour);
+  height: 25rem;
+  padding: 2rem;
+  box-shadow: 0px 25px 50px rgba(22, 25, 79, 0.05);
+  h4 {
+    font-size: 3rem;
+    color: var(--purple-primary);
+  }
+  p {
+    color: black;
+  }
+`
 
 export default Form
--- src/components/Founders.js
+++ src/components/Founders.js
@@ -48,6 +48,18 @@ function Founders() {
 }
 
 const FoundersStyled = styled.header`
+@media screen and (max-width: 1347px) {
+    padding: 5rem 14rem;
+  }
+  @media screen and (max-width: 1186px) {
+    padding: 5rem 8rem;
+  }
+  @media screen and (max-width: 990px) {
+    padding: 5rem 4rem;
+  }
+  @media screen and (max-width: 760px) {
+    padding: 5rem 4rem;
+  }
     .left-items{
         position: relative;
         padding-right: 2rem;
